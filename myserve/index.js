9cbd825b40b57fb0f4d6ad942300d400ca1cdd3d
karol <ka610313@gmail.com>
2023-11-30 16:25:06 -0300

    .

--- myserve/index.js
+++ myserve/index.js
@@ -2,13 +2,37 @@ const express = require('express');
 const app=express();
 const host = 'localHost';
 const port = 8080;
-//rota
+const cookieParser = require('cookie-parser');
+
+app.use(cookieParser())
+app.use(express.urlencoded({extended: true}))
+//roats POST
+app.post('/',( req,res) =>{
+    res.send(req.body.message +" - "+ req.body.nome)
+})
+app.post('/echo',( req,res) =>{
+    res.send("ECHO "+req.body.message +" - "+ req.body.nome)
+})
+app.post('/echo/:message',( req,res) =>{
+    res.send("ECHO "+req.params.message )
+})
+//rota GET
+
 app.get('/', (req,res)=>{
-    res.send('request received');
+    res.type('request received');
+    if(req.cookies.known === undefined){
+        res.cookie('known','1');
+        res.send('seja bem vindo , novo visitante!');
+    }else{
+        res.send('seja bem vindo novamente, visitante!')
+    }
 });
 
 app.get('/mensagem', (req,res)=>{
     res.send('chega natal !!');
+});
+app.get('/curso', (req,res)=>{
+    res.send('tecnico de informatica para internet');
 });
 
 app.listen(port,host,() =>{
