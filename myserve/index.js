2fd9e7dd2147ed61764f958bdccc3c6118ff33e1
karol <ka610313@gmail.com>
2023-11-30 14:38:16 -0300

    .

--- myserve/index.js
+++ myserve/index.js
@@ -1,3 +1,16 @@
 const express = require('express');
 const app=express();
-const host = 'localHost'
+const host = 'localHost';
+const port = 8080;
+//rota
+app.get('/', (req,res)=>{
+    res.send('request received');
+});
+
+app.get('/mensagem', (req,res)=>{
+    res.send('chega natal !!');
+});
+
+app.listen(port,host,() =>{
+    console.log(`server ready at http://${host}:${port}`)
+})
